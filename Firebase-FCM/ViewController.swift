1bb055a3abf56fbf80e488c2c68a9243603a75ab
Doron Katz <doron@doronkatz.com>
2018-10-24 18:36:56 -0700

    Initial Commit

---
+++ Firebase-FCM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-FCM
+//
+//  Created by Doron Katz on 10/24/18.
+//  Copyright © 2018 Doron Katz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
